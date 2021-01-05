d3e8c593fceafc9b5a6674e92c4111b3fafb94df
pranayagra <pranay.agra@gmail.com>
2021-01-04 21:38:33 -0500

    base outline

---
+++ src/teambot/battlecode2021/util/Pathfinding.java
@@ -0,0 +1,16 @@
+package teambot.battlecode2021.util;
+
+import battlecode.common.*;
+import teambot.battlecode2021.PoliticanBot;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Pathfinding {
+
+    private static RobotController controller;
+
+    public static void init(RobotController controller) {
+        Pathfinding.controller = controller;
+    }
+}
