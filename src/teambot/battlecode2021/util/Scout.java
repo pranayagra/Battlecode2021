8c583ec1e7a9bb89b4e4d8ab4f76b454151c7d52
Mark Li <mark.haoxiang@gmail.com>
2021-01-17 15:04:36 +0800

    Moving EC scout code to general and comm scheduling system

--- src/teambot/battlecode2021/util/Scout.java
+++ src/teambot/battlecode2021/util/Scout.java
@@ -29,22 +29,11 @@ public class Scout {
     }
 
     // Checks edge. If not on map, set cache and send flag
+    // Changed to one function instead of four
     private static void checkEdge(Direction dir, MapLocation center, int radius) throws GameActionException {
 
-        int x = 0;
-        int y = 0;
-        if (dir == Direction.NORTH) {
-            y = 1;
-        }
-        else if (dir == Direction.SOUTH) {
-            y = -1;
-        }
-        else if (dir == Direction.EAST) {
-            x = 1;
-        }
-        else {
-            x = -1;
-        }
+        int x = dir.getDeltaX();
+        int y = dir.getDeltaY();
         if (controller.onTheMap(center.translate(x*radius, y*radius))) return; //the furthest distance is already on the map (so the remaining must also be)
         int flag;
         for (int i = radius - 1; i != 0; --i) {
@@ -72,5 +61,62 @@ public class Scout {
             }
         }
     }
+
+    // Check for ECs
+
+    /* Find all ECs in sensor range and communicate it if and only if
+    *  1) the bot has never seen it before OR
+    *  2) the bot previously scouted it but has since changed teams (ally, enemy, neutral)
+    * */
+
+    //TODO: Not sure if hashmap best data structure. Bytecode inefficient - change to array?
+    private void scoutECs() throws GameActionException {
+        MapLocation currentLocation = controller.getLocation();
+        RobotInfo[] nearbyRobots = controller.senseNearbyRobots();
+        for (RobotInfo info : nearbyRobots) { 
+            if (info.type == RobotType.ENLIGHTENMENT_CENTER) {
+                Debug.printInformation( "checking EC location " + info.location + " => ", "");
+                CommunicationLocation.FLAG_LOCATION_TYPES locationTypePrevious = Cache.FOUND_ECS.get(info.location);
+                CommunicationLocation.FLAG_LOCATION_TYPES locationTypeNew = getECType(info.team);
+                if (locationTypePrevious == null || locationTypePrevious != locationTypeNew) { //if null or if the type of EC has since changed
+                    Cache.FOUND_ECS.put(info.location, locationTypeNew); //overwrite or add
+                    int flag = CommunicationECInfo.encodeECInfo(
+                        false, true, getCommunicatedUnitTeamForECInfo(info.team), info.influence);
+                    Debug.printInformation(locationTypeNew + " AT LOCATION " + info.location + " => ", flag);
+                    Comms.checkAndAddFlag(flag);
+                }
+            }
+        }
+    }
     
+    /* Converts the EC Team to a Location Flag Type for communication purposes */
+    private CommunicationLocation.FLAG_LOCATION_TYPES getECType(Team ECTeam) {
+        if (ECTeam.equals(Cache.OUR_TEAM)) {
+            return CommunicationLocation.FLAG_LOCATION_TYPES.MY_EC_LOCATION;
+        } else if (ECTeam.equals(Cache.OPPONENT_TEAM)) {
+            return CommunicationLocation.FLAG_LOCATION_TYPES.ENEMY_EC_LOCATION;
+        } else {
+            return CommunicationLocation.FLAG_LOCATION_TYPES.NEUTRAL_EC_LOCATION;
+        }
+    }
+
+    private CommunicationECInfo.COMMUNICATION_UNIT_TEAM getCommunicatedUnitTeamForECInfo(Team ECTeam) {
+        if (ECTeam.equals(Cache.OUR_TEAM)) {
+            return CommunicationECInfo.COMMUNICATION_UNIT_TEAM.MY;
+        } else if (ECTeam.equals(Cache.OPPONENT_TEAM)) {
+            return CommunicationECInfo.COMMUNICATION_UNIT_TEAM.ENEMY;
+        } else {
+            return CommunicationECInfo.COMMUNICATION_UNIT_TEAM.NEUTRAL;
+        }
+    }
+
+    private CommunicationRobotID.COMMUNICATION_UNIT_TEAM getCommunicatedUnitTeamForRobotID(Team ECTeam) {
+        if (ECTeam.equals(Cache.OUR_TEAM)) {
+            return CommunicationRobotID.COMMUNICATION_UNIT_TEAM.MY;
+        } else if (ECTeam.equals(Cache.OPPONENT_TEAM)) {
+            return CommunicationRobotID.COMMUNICATION_UNIT_TEAM.ENEMY;
+        } else {
+            return CommunicationRobotID.COMMUNICATION_UNIT_TEAM.NEUTRAL;
+        }
+    }
 }
